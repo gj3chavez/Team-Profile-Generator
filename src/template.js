a81f857e97bc4913f7d2333eda767757156c05e0
Giana <gj3chavez@gmail.comgit config --global user.emailpwd>
2023-02-20 19:23:15 -0600

    updates template

--- src/template.js
+++ src/template.js
@@ -3,10 +3,11 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
-const generateHTML = () =>
+const generateHTML = (team) => `
 
 <!DOCTYPE html> 
 <html lang="en">
+
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,12 +15,12 @@ const generateHTML = () =>
         <title>Team Profile</title>
         <link rel="stylesheet" href="style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <script src="https://kit.fontawesome.com/65ce8f9f8f.js" crossorigin="anonymous"></script>
-                
+        <script src="https://kit.fontawesome.com/65ce8f9f8f.js" crossorigin="anonymous"></script>         
     </head>
+    
     <body>
         <header>
-        <!-- As a heading -->
+        
             <nav class="navbar bg-body-tertiary">
                 <div class="container-fluid justify-content-center" id="card-body">
                     <span class="navbar-brand mb-0 h1">Team Profile</span>
@@ -28,92 +29,79 @@ const generateHTML = () =>
         </header>
     <main>
         <div class="container">
-            <div class = "row justify-content-center">
-        
-            <div class="card" style="width: 18rem;">
-            <!-- <img src="..." class="card-img-top" alt="..."> -->
-            <div class="card-body">
-              <h5 class="card-title">Albus Dumbledore</h5>
-              <p class="card-text"><i class="fa-solid fa-mug-hot"></i> Manager</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">ID: 123</li>
-              <li class="list-group-item">Email: dumbledore@gmail.com</li>
-              <li class="list-group-item">Office number: 8</li>
-            </ul>
-            <!-- <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div> -->
-          </div>
-
-          <div class="card" style="width: 18rem;">
-            <!-- <img src="..." class="card-img-top" alt="..."> -->
-            <div class="card-body">
-              <h5 class="card-title">Harry Potter</h5>
-              <p class="card-text"><i class="fa-solid fa-glasses"></i> Engineer</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">ID: 2</li>
-              <li class="list-group-item">Email: harry-potter@gmail.com</li>
-              <li class="list-group-item">GitHub: harry-pott</li>
-            </ul>
-            <!-- <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div> -->
-          </div>
-
-
-          <div class="card" style="width: 18rem;">
-            <!-- <img src="..." class="card-img-top" alt="..."> -->
-            <div class="card-body">
-              <h5 class="card-title">Ron Weasley</h5>
-              <p class="card-text"><i class="fa-solid fa-glasses"></i> Engineer</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">ID: 3</li>
-              <li class="list-group-item">Email:ronweas@gmail.com</li>
-              <li class="list-group-item">GitHub: ronweas</li>
-            </ul>
-            <!-- <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div> -->
-          </div>
-
-
-          <div class="card" style="width: 18rem;">
-            <!-- <img src="..." class="card-img-top" alt="..."> -->
-            <div class="card-body">
-              <h5 class="card-title">Hermione Granger</h5>
-              <p class="card-text"><i class="fa-solid fa-glasses"></i> Engineer</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">ID: 4</li>
-              <li class="list-group-item">Email: hermioneg@gmail.com</li>
-              <li class="list-group-item">GitHub: hermioneg</li>
-            </ul>
-            <!-- <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div> -->
-          </div>
-
-
-          <div class="card" style="width: 18rem;">
-            <!-- <img src="..." class="card-img-top" alt="..."> -->
-            <div class="card-body">
-              <h5 class="card-title">Neville Longbottom</h5>
-              <p class="card-text"><i class="fa-solid fa-graduation-cap"></i> Intern</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">ID: 5</li>
-              <li class="list-group-item">Email: nevillel@gmail.com</li>
-              <li class="list-group-item">GitHub: nevillel</li>
-            </ul>
-            <!-- <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div> -->
+            <div class = "row justify-content-center"></div>
+            ${createCards(team)}
+          </div> 
         </div>
+    </main>
+  </body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+</html>`
+
+const createManagerCard = (Manager) =>
+  `<div class="card" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title">${Manager.getName()}/h5>
+  <p class="card-text"><i class="fa-solid fa-users"></i> ${Manager.getRole()}</p>
+</div>
+<ul class="list-group list-group-flush">
+  <li class="list-group-item">ID: ${Manager.getId()}</li>
+  <li class="list-group-item">Email: <a href="mailto:${Manager.getEmail()}">${Manager.getEmail()}</a><li>
+  <li class="list-group-item">${Manager.getOfficeNumber()}</li>
+</ul>
+</div>`
+
+
+const createEnginerCard = (Engineer) =>
+  `<div class="card" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title">${Enginner.getName()}/h5>
+  <p class="card-text"><i class="fa-solid fa-users"></i> ${Engineer.getRole()}</p>
+</div>
+<ul class="list-group list-group-flush">
+  <li class="list-group-item">ID: ${Engineer.getId()}</li>
+  <li class="list-group-item">Email:<a href="mailto:${Engineer.getEmail()}">${Engineer.getEmail()}</a><li>
+  <li class="list-group-item"><a href="https://github.com/${Engineer.getGithub()}" target="_blank"
+  rel="noopener noreferrer">${Engineer.getGithub()}</a></li>
+</ul>
+</div>`
+
+
+const createInternCard = (Intern) =>
+  `<div class="card" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title">${Intern.getName()}/h5>
+  <p class="card-text"><i class="fa-solid fa-users"></i> ${Intern.getRole()}</p>
+</div>
+<ul class="list-group list-group-flush">
+  <li class="list-group-item">ID: ${Intern.getId()}</li>
+  <li class="list-group-item">Email:<a href="mailto:${Intern.getEmail()}">${Intern.getEmail()}</a><li>
+  <li class="list-group-item">${Intern.getSchool()}</li>
+</ul>
+</div>`
+
+
+function createCards(team) {
+  let cards = []
+  for (let i = 0; i < team.length; i++) {
+    const thisEmployee = team[i];
+    switch (thisEmployee.getRole()) {
+      case 'Manager':
+        const manager = new Manager(thisEmployee.id, thisEmployee.name, thisEmployee.email, thisEmployee.getOfficeNumber);
+        cards.push(createManagerCard(manager));
+        break;
+      case 'Engineer':
+        const engineer = new Enginner(thisEmployee.id, thisEmployee.name, thisEmployee.email, thisEmployee.github);
+        cards.push(createEngineerCard(engineer));
+        break;
+      case 'Intern':
+        const intern = new Intern(thisEmployee.id, thisEmployee.name, thisEmployee.email, thisEmployee.school);
+        cards.push(createInternCard(intern));
+        break;
+    }
+  }
+  return cards.join(``)
+}
+
+module.exports = generateHTML;
